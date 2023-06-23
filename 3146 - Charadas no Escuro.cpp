c4392857e38dec7045a3b2be1a8cbcc4fe68082a
Quezad4 <105978156+Quezad4@users.noreply.github.com>
2023-06-23 03:22:37 -0300

    Create  3146 - Charadas no Escuro.cpp

---
+++ 3146 - Charadas no Escuro.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	float R, pi, circ;
+	pi = 3.14;
+	cin >> R;
+	circ = 2*pi*R;
+	cout << fixed;
+	cout.precision(2);
+	cout << circ << endl;
+	
+}
