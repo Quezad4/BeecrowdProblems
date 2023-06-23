9dc6a4de6c9a04e4d48c089922f26e8b693a3b60
Quezad4 <105978156+Quezad4@users.noreply.github.com>
2023-06-23 03:34:10 -0300

    Create  2424 - Tira-teima.cpp

---
+++ 2424 - Tira-teima.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int X, Y;
+	cin >> X >> Y;
+	if(Y>468 or Y<0 or X>432 or X<0){
+		cout << "fora" << endl;
+	}
+	else{
+		cout << "dentro" << endl;
+	} 
+}
