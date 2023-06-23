c705181a98e090335ba5beeb1d40c6bb33900e37
Quezad4 <105978156+Quezad4@users.noreply.github.com>
2023-06-23 03:54:59 -0300

    Create 3303 - Palavrão.cpp

---
+++ 3303 - Palavrão.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string a; cin >> a;
+	if(a.size()>=10){
+		cout << "palavrao" << endl;
+	}
+	else{
+		cout << "palavrinha" << endl;
+	}
+	
+}
