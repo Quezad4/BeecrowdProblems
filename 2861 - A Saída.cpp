391e0651d10be44a5e005d4f9f0c75e1f827ca68
Quezad4 <105978156+Quezad4@users.noreply.github.com>
2023-06-23 03:21:51 -0300

    Create 2861 - A Saída.cpp

---
+++ 2861 - A Saída.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main(){
+	string pergunta;
+	int N;
+	cin >> N;
+	for(int x; x<N; x++){
+		cin>> pergunta;
+		cout << "gzuz" << endl;
+	}
+}
