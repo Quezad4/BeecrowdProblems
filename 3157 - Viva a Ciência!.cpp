b310727ffd872d6b70c5d6c38467cf26f95e5a2d
Quezad4 <105978156+Quezad4@users.noreply.github.com>
2023-06-23 03:23:11 -0300

    Create  3157 - Viva a Ciência!.cpp

---
+++ 3157 - Viva a Ciência!.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main (){
+	setlocale(LC_ALL,"");
+	string texto;
+	cin>> texto;
+	cout << "Nao se refuta ciencia com opiniao" << endl;
+}
